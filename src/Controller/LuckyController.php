8eb3ea6f7774f564a3315b9c462349ade9322ab7
sophie <sophie@mail.com>
2021-02-11 09:18:49 +0100

    mercredi

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,11 +2,14 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController
+class LuckyController extends AbstractController
 {
+
+
     /**
      * @Route("/lucky/number")
      */    
