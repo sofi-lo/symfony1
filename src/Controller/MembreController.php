57ad682dd23d1195071ae91446b503c70c74930a
sophie <sophie@mail.com>
2021-02-16 16:27:30 +0100

    annonce

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -6,13 +6,41 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+// ne pas oublier les use
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Annonce;
+use App\Form\AnnonceType;
+
 class MembreController extends AbstractController
 {
-    #[Route('/membre', name: 'membre')]
-    public function index(): Response
+    #[Route('/membre', name: 'membre', methods: ['GET', 'POST'])]
+    public function index(Request $request): Response
     {
+        $annonce = new Annonce();
+        $form = $this->createForm(AnnonceType::class, $annonce);
+        $form->handleRequest($request);
+
+        $messageConfirmation = "";
+        if ($form->isSubmitted() && $form->isValid()) {
+            // compléter les infos manquantes
+            $annonce->setDatePublication(new \DateTime());
+            // https://symfony.com/doc/current/security.html#a-fetching-the-user-object
+            // ajouter l'auteur de l'annonce avec l'utilisateur connecté
+            $userConnecte = $this->getUser();
+            $annonce->setUser($userConnecte);
+            
+            // code qui insère la nouvelle ligne dans SQL
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($annonce);
+            $entityManager->flush();
+
+            $messageConfirmation = "votre annonce est publiée";
+        }
+
         return $this->render('membre/index.html.twig', [
-            'controller_name' => 'MembreController',
+            'annonce' => $annonce,
+            'form' => $form->createView(),
+            'messageConfirmation' => $messageConfirmation,
         ]);
     }
 }
--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Newsletter;
 use App\Form\NewsletterType;
-
+use App\Repository\AnnonceRepository;
 class SiteController extends AbstractController
 
 {
@@ -75,6 +75,12 @@ class SiteController extends AbstractController
             'controller_name' => 'SiteController',
         ]);
     }
-
+    #[Route('/annonces', name: 'annonces', methods: ['GET'])]
+    public function annonces(AnnonceRepository $annonceRepository): Response
+    {
+        return $this->render('site/annonces.html.twig', [
+            'annonces' => $annonceRepository->findAll(),    // SELECT * FROM annonces
+        ]);
+    }
 }
 
